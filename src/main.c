71666c8edd5360c4dfbc2fa736bbfef78f136dfd
root <root@localhost>
2023-09-10 13:05:34 +0800

    auto publish 1694322334276

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    /*below is a program that will print an
+    output of a sample short story. 
+    */
+   printf("Short Story: \n\n");
+    char characterName [ ] = "Aurora";
+    int characterAge = 17;
+    printf("There was once a girl named %s.\n", characterName);
+    printf("She lived in a small village, called Townsville\n");
+    printf("She was %d years old but quite matured at herage.\n\n", characterAge);
+    printf("%s spent most of her time at home readingand tending her orchard.\n", characterName);
+    printf("She lived simply and was honestly contented  to what she has in life.\n");
+    printf("She was hopeful and bright young lady and as resplendent as  to her name  suggests, the \t \'Northern Lights\'.\n\n\n\n");
+   
+    return 0;
+
+}  
+    
